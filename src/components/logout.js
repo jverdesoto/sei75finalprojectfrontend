2cb21683ae97d528d8ac65e8b6992c7181eabbe7
jverdesoto <verdesoto.diseno@gmail.com>
2023-11-21 22:26:47 -0500

    modified logout v6

--- src/components/logout.js
+++ src/components/logout.js
@@ -4,16 +4,14 @@ export const Logout = () => {
   useEffect(() => {
     (async () => {
       try {
-        await axios.post(
+        // eslint-disable-next-line no-unused-vars
+        const { data } = await axios.post(
           `${process.env.REACT_APP_BACKEND_URL}/logout/`,
           {
             refresh_token: localStorage.getItem("refresh_token"),
           },
-          {
-            headers: {
-              "Content-Type": "application/json"
-            }
-          }
+          { headers: { "Content-Type": "application/json" } },
+          { withCredentials: true }
         );
         localStorage.clear();
         axios.defaults.headers.common["Authorization"] = null;
